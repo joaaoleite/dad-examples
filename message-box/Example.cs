b59d415e506a1a48cf4f17cab215216c999ded2a
joaaoleite <joaogsleite@gmail.com>
2016-09-25 22:51:45 +0100

    message box example

---
+++ message-box/Example.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Example{
+
+	public class Example{
+
+		public Example (){
+			System.Console.WriteLine("Quem?");
+		}
+
+		static void Main(string[] args){
+
+			string message = "Do you like Visual Studio?";
+			string caption = "Example";
+
+			MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+			DialogResult result;
+
+			// Displays the MessageBox.
+			result = MessageBox.Show(message, caption, buttons);
+
+			if (result == System.Windows.Forms.DialogResult.Yes){
+				System.Console.WriteLine(args.Length);
+			}
+		}
+	}
+}
+
