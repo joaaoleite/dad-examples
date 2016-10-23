d1acd6d86d3c557fd05afdb4e57390108cab1fc3
joaaoleite <joaogsleite@gmail.com>
2016-10-23 23:59:48 +0100

    halib lab1

---
+++ halib/HelloWorld/HelloWorld/HelloWorld.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class HelloWorld
+{
+    
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+
+}
---
+++ halib/NameList/NameList/IListaNomes.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace NameList
+{
+    public partial class IListaNomes : Form
+    {
+
+        private ArrayList names;
+
+        public IListaNomes()
+        {
+            names = new ArrayList();
+            InitializeComponent();
+        }
+
+        private void IListaNomes_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            names.Add(textbox.Text);
+            textbox.Text = "";
+            MessageBox.Show("Name added!");
+        }
+
+        private void show_Click(object sender, EventArgs e)
+        {
+            String list = "";
+            foreach(String item in names)
+            {
+                list = list + "\n" + item;
+            }
+            ListWindow listWindow = new ListWindow(list);
+            listWindow.Show();
+        }
+
+        private void erase_Click(object sender, EventArgs e)
+        {
+            names.Clear();
+            MessageBox.Show("List erased!");
+        }
+    }
+}
